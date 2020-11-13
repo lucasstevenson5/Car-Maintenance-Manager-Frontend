da88017162cd08e39546e0b2b8b0b26a7931a691
LJ Stevenson <StevensonLucasJ@JohnDeere.com>
2020-11-13 10:36:25 -0600

    login and signup styling

--- src/components/Login-Signup/Login.js
+++ src/components/Login-Signup/Login.js
@@ -18,28 +18,47 @@ class Login extends Component {
 
     render() {
         return (
-            <div>
-                <h1>Log In Page</h1>
-                {this.props.error && <div>{this.props.error}</div>}<br />
-                <form onSubmit={(e) => this.props.handleLogin(e, this.state)}>
-                    Username: <input className="border-solid border-2 border-gray-900"
-                        type="text"
-                        name="username"
-                        placeholder="username"
-                        value={this.state.username}
-                        onChange={this.updateForm}
-                    /><br /><br />
-                    Password: <input className="border-solid border-2 border-gray-900"
-                        type="password"
-                        name="password"
-                        placeholder="password"
-                        value={this.state.password}
-                        onChange={this.updateForm}
-                    /><br /><br />
-                    <input type="submit" value="Login"
-                        className="border-solid border-2 border-gray-900"
-                    />
-                </form>
+            <div className="bg-gainsboro">
+                <h1 className="text-center">Log In Page</h1>
+                <div className="flex flex-wrap justify-center">
+                    {this.props.error && <div>{this.props.error}</div>}<br />
+                    <form onSubmit={(e) => this.props.handleLogin(e, this.state)}
+                        className="mb-4 text-center w-3/5 border-solid border-2 border-gray-900 bg-blackcoral max-w-sm m-auto"
+                    >
+                        <div className="my-2">
+                            <span className="text-gainsboro">Username: </span>
+                            <input 
+                                className="border-solid border-2 border-gray-900"
+                                type="text"
+                                name="username"
+                                placeholder="username"
+                                value={this.state.username}
+                                onChange={this.updateForm}
+                            /><br /><br />
+                        </div>
+                        <div className="mb-2">
+                            <span className="text-gainsboro">Password: </span>
+                            <input 
+                                className="border-solid border-2 border-gray-900"
+                                type="password"
+                                name="password"
+                                placeholder="password"
+                                value={this.state.password}
+                                onChange={this.updateForm}
+                            /><br /><br />
+                        </div>
+                        <div className="mb-2">
+                            <input 
+                                type="submit" 
+                                value="Login"
+                                className="border-solid border-b-4 border-2 border-starblue hover:text-gray-400 
+                                hover:underline bg-gainsboro hover:bg-starblue p-2 text-starblue
+                                rounded m-2 hover:border-black"
+                            />
+                        </div>
+                    </form>
+                </div>
+                
             </div>
         )
     }
--- src/components/Login-Signup/Signup.js
+++ src/components/Login-Signup/Signup.js
@@ -33,40 +33,66 @@ class Signup extends Component {
 
     render() {
         return (
-            <div>
-                <h1>Sign Up Page</h1>
-                {this.state.error && <div>{this.state.error}</div>}<br />
-                <form onSubmit={(e) => this.props.handleSignup(e, this.state)}>
-                    Full Name: <input className="border-solid border-2 border-gray-900"
-                        type="text"
-                        name="name"
-                        placeholder="Your name... duh"
-                        value={this.state.name}
-                        onChange={this.updateForm}
-                    /><br /><br />
-                    Username: <input className="border-solid border-2 border-gray-900"
-                        type="text"
-                        name="username"
-                        placeholder="username"
-                        value={this.state.username}
-                        onChange={this.updateForm}
-                    /><br /><br />
-                    Password: <input className="border-solid border-2 border-gray-900"
-                        type="password"
-                        name="password"
-                        placeholder="password"
-                        value={this.state.password}
-                        onChange={this.updateForm}
-                    /><br /><br />
-                    {/* <input
-                        type="password"
-                        name="passwordConfirm"
-                        placeholder="confirm password"
-                        value={this.state.passwordConfirm}
-                        onChange={this.updateForm}
-                    /> */}
-                    <input type="submit" value="Create Profile" />
-                </form>
+            <div className="bg-gainsboro">
+                <h1 className="text-center">Sign Up Page</h1>
+                <div className="flex flex-wrap justify-center">
+                    {this.state.error && <div>{this.state.error}</div>}<br />
+                    <form onSubmit={(e) => this.props.handleSignup(e, this.state)}
+                        className="mb-4 text-center w-3/5 border-solid border-2 border-gray-900 bg-blackcoral max-w-sm m-auto"
+                    >
+                        <div className="my-2">
+                            <span className="text-gainsboro">Full Name: </span>
+                            <input 
+                                className="border-solid border-2 border-gray-900"
+                                type="text"
+                                name="name"
+                                placeholder="Your name... duh"
+                                value={this.state.name}
+                                onChange={this.updateForm}
+                            /><br /><br />
+                        </div>
+                        <div className="mb-2">
+                            <span className="text-gainsboro">Username: </span>
+                            <input 
+                                className="border-solid border-2 border-gray-900"
+                                type="text"
+                                name="username"
+                                placeholder="username"
+                                value={this.state.username}
+                                onChange={this.updateForm}
+                            /><br /><br />
+                        </div>
+                        <div className="mb-2">
+                            <span className="text-gainsboro">Password: </span>
+                            <input 
+                                className="border-solid border-2 border-gray-900"
+                                type="password"
+                                name="password"
+                                placeholder="password"
+                                value={this.state.password}
+                                onChange={this.updateForm}
+                            /><br /><br />
+                        </div>
+                        {/* <input
+                            type="password"
+                            name="passwordConfirm"
+                            placeholder="confirm password"
+                            value={this.state.passwordConfirm}
+                            onChange={this.updateForm}
+                        /> */}
+                        <div className="mb-2">
+                            <input 
+                                type="submit" 
+                                value="Create Profile" 
+                                className="border-solid border-b-4 border-2 border-starblue hover:text-gray-400 
+                                hover:underline bg-gainsboro hover:bg-starblue p-2 text-starblue
+                                rounded m-2 hover:border-black"
+                            />
+                        </div>
+                        
+                    </form>
+                </div>
+                
             </div>
         )
     }
